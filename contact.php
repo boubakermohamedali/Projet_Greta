d0a9610fe098f243e43d42dd834f48b694340bad
boubakermohamedali <72795659+boubakermohamedali@users.noreply.github.com>
2023-06-16 02:53:09 -0700

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,21 @@
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Contact</title>
+</head>
+<body>
+<h1>Contact</h1>
+<p>Une suggestion? Une demande? Un problème? N'hésitez pas à nous écrire!</p>
+<br>
+<form method="POST">
+   
+    <p><label for="username">Nom d'utilisateur</label>
+        <input type="text" name="username" id="username" required></p>
+    <br>
+    <p><label for="email">Email</label>
+        <input type="email" name="email" id="email" required></p>
+    <br>
+    <p><label for="message">Veuillez saisir votre message:</label>
+        <input type="textarea" name="message" id="message" required></p>
+    <input type="submit" value="Envoyer">
+
